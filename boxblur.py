847f01c472089a852fef10f541e6042fcbd32a0b
garbage1010 <74822693+garbage1010@users.noreply.github.com>
2023-05-16 13:45:33 -0500

    Update boxblur.py

--- boxblur.py
+++ boxblur.py
@@ -1,6 +1,16 @@
 import numpy
 import cv2
-
+#define original image with path
+path =
+#read image
+original_image = cv2.imread(path)
+#create the box blur kernel and operation
 def boxblur
-  
 
+#show user images
+cv2.imshow("Original", original_image)
+cv2.imshow("Blurred", edited_image)
+
+#close windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
