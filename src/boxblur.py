d982314d3e5f91f3b1c50ea57c9bb9a36b628c95
garbage1010 <74822693+garbage1010@users.noreply.github.com>
2023-05-17 09:02:08 -0500

    Update boxblur.py

--- src/boxblur.py
+++ src/boxblur.py
@@ -1,11 +1,22 @@
 import numpy
 import cv2
+from PIL import Image, ImageFilter
 #define original image with path
 path = r'C:\Users\rory5\OneDrive\Desktop\code\PYTHON\house.tiff'
 #read image
 original_image = cv2.imread(path)
 #create the box blur kernel and operation
-#def boxblur
+def boxblur(path, radius=1):
+  img = Image.open(path).convert(RGB)
+  edited_image = img.copy()
+  width, height = img.size
+  area = (2*radius+1)**2
+  for x in range(r, width-r):
+    for y in range(r, height-r):
+      sum_pixels(0, 0, 0)
+      
+  
+
 
 #show user images
 cv2.imshow("Original", original_image)
