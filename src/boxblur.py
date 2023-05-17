8ea101a177f6befc48e76dc445839adec6fa878d
RoryM0 <rory5827@outlook.com>
2023-05-16 20:37:38 -0500

    Update boxblur.py

--- src/boxblur.py
+++ src/boxblur.py
@@ -1,15 +1,15 @@
 import numpy
 import cv2
 #define original image with path
-path =
+path = r'C:\Users\rory5\OneDrive\Desktop\code\PYTHON\house.tiff'
 #read image
 original_image = cv2.imread(path)
 #create the box blur kernel and operation
-def boxblur
+#def boxblur
 
 #show user images
 cv2.imshow("Original", original_image)
-cv2.imshow("Blurred", edited_image)
+#cv2.imshow("Blurred", edited_image)
 
 #close windows
 cv2.waitKey(0)
